dbe4217b25ec168f574a800ebb7859ba249fdabe
jakson <hjobs2007@gmail.com>
2026-01-21 14:04:37 +0800

    202601021Wa

--- 15ORM_sqlalchemy_alembic/05多表关系/schemas.py
+++ 15ORM_sqlalchemy_alembic/05多表关系/schemas.py
@@ -3,20 +3,44 @@
 # @File    : schemas.py
 from decimal import Decimal
 from datetime import date
+from typing import Optional
+
 from pydantic import BaseModel,Field
 from models.employee import GenderValue
-
+from  models.department import Department
 class Empschema(BaseModel):
-    id:int=Field(description='员工ID,添加数据的时候不用传递')
+    id:int=Field(description='员工ID,添加数据的时候不用传递',default=None)
     name:str=Field(description='姓名')
-    salary:Decimal=Field(description='薪资',default=None)
+    salary:Optional[float]=Field(description='薪资',default=None)
     bonus:int=Field(description='奖金',default=None)
-    gender:GenderValue=Field(description='性别')
+    gender:GenderValue=Field(description='性别',default=GenderValue.MALE)
     is_leave:bool=Field(description='是否离职,True为离职',default=False) #默认不离职
-    dep_id:int = Field(description='员工所属部门id',default=None)
+    dep_id:Optional[int] = Field(description='员工所属部门id',default=None)
     entry_date:date=Field(description='入职时间',default=None)
 
     #important：这是关键,需要重写才可以实现sqlalchemy的模型和pydantic模型之间的转换
+    class Config:
+        from_attributes=True #important:有了这个配置才能把ORM模型转换为Pydantic模型对象
+
+
+
+#tips:定义创建部门的schema,方便下面返回详细信息的时候调用此表的schema
+class  DepartmentSchema(BaseModel):
+    id:int=Field(description='员工部门的ID,添加数据的时候不要传',default=None)
+    name:str=Field(description='部门名称',default=None)
+    city:str=Field(description='部门所在城市的名称',default=None)
+
+    class  Config:
+        from_attributes=True
+
+#tips：获取员工的详细信息,包含部门名称,只需要继承上面的Schema并新增一个部门名称即可,注意也要与employee表保持一致
+# important:注意因为我们在employee表中定义的dep_name是属于Department类型的,所以这里也得是Department类型
+
+class DetailEMPINFO(Empschema):
+    '''
+    响应员工信息的模型类,包含该员工的所属部门
+    '''
+    dep_name:Optional[DepartmentSchema]= Field(description='所属部门',default=None)
     class Config:
         from_attributes=True
 
--- 15ORM_sqlalchemy_alembic/05多表关系/views.py
+++ 15ORM_sqlalchemy_alembic/05多表关系/views.py
@@ -2,36 +2,49 @@
 # @Author  : hero
 # @File    : views.py
 from http.client import HTTPException
-
-from certifi import where
-from fastapi import APIRouter,Path,Query,Body,Depends,HTTPException,status
+from fastapi import APIRouter, Depends, HTTPException,Query
 from sqlalchemy import Select
 from sqlalchemy.orm import Session
 from typing import Annotated
 
 from databases import get_session
-from schemas import Empschema
+from schemas import Empschema, DepartmentSchema, DetailEMPINFO
 from models import Employee
 
-emp_apirt = APIRouter(prefix='/api/employee',tags=['员工管理'])
+emp_apirt = APIRouter(prefix='/employee', tags=['员工管理'])
 
-@emp_apirt.get('/')
-async def getemp():
-    pass
 
-@emp_apirt.post('/emp/')
-async def create_emp(employee:Empschema,session:Annotated[Session,Depends(get_session)]):
-    #step1:先判断数据库中有没有重复的数据,即先进行查询
-    is_emp =  session.execute(Select(Employee.name).where(Employee.name==employee.name))
-    if is_emp: #tips:如果不为空,即已存在重复名字的员工
-        raise HTTPException(status_code=405,detail='重复录入,已经存在该员工的信息')
+# tips:根据传入的员工id返回详细的员工信息
+@emp_apirt.get('/emp/{id}', response_model=DetailEMPINFO)
+async def getemp(id: int, session: Annotated[Session, Depends(get_session)]):
+    result = session.get(Employee, id)
+    if result:
+        return result
+    else:
+        raise HTTPException(status_code=403, detail='未查讯到讯息')
 
-    #tips:需要把Pydantic的模型对象(Empschema)转换为ORM的模型对象(Employee),
-    #important:所以为了解决这个问题,我们刚才在定义Empschema的时候重写了Config将其设置为了from_attributes=True
-    db_emp = Employee(**employee.model_dump()) #tips:这里和tortoiseorm差不多
-    #tips:其实这里就跟curd操作当中创建员工对象的操作是一样的,就是新建一个员工对象然后**employee.model_dump()就是解包赋值
+#tips:分页查询员工信息
+
+@emp_apirt.get('/emp',response_model=list[DetailEMPINFO],description='查询所有员工列表')
+async def getall_emps(session: Annotated[Session, Depends(get_session)],offset:int=Query(title='输入的是offset',default=0),page_size:int=Query(default=5,description='每页最多显示的数量')):
+
+    return session.scalars(Select(Employee).offset(offset).limit(page_size)).all()
+
+
+
+# tips:新增员工
+@emp_apirt.post('/emp/', description='添加员工', response_model=Empschema)
+async def create_emp(employee: Empschema, session: Annotated[Session, Depends(get_session)]):
+    # step1:先判断数据库中有没有重复的数据,即先进行查询
+    is_emp = session.execute(Select(Employee.name).where(Employee.name == employee.name)).first()
+    if is_emp:  # tips:如果不为空,即已存在重复名字的员工
+        raise HTTPException(status_code=405, detail='重复录入,已经存在该员工的信息')
+
+    # tips:需要把Pydantic的模型对象(Empschema)转换为ORM的模型对象(Employee),
+    # important:所以为了解决这个问题,我们刚才在定义Empschema的时候重写了Config将其设置为了from_attributes=True
+    db_emp = Employee(**employee.model_dump())  # tips:这里和tortoiseorm差不多
+    # tips:其实这里就跟curd操作当中创建员工对象的操作是一样的,就是新建一个员工对象然后**employee.model_dump()就是解包赋值
 
     session.add(db_emp)
-    session.commit()  #important:之前我们使用的是sessionmaker来做的,所以会自动提交,但这次是我们手动管理session,所以需要提交
-
-
+    session.commit()  # important:之前我们使用的是sessionmaker来做的,所以会自动提交,但这次是我们手动管理session,所以需要提交
+    return db_emp
